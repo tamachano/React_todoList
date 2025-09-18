581a3cd53ad771f0fc34c676cd99208725142263
tamachano <i.cclimber.luigi@gmail.com>
2025-09-18 10:37:04 +0900

    更新内容：〇〇機能追加

--- src/App.js
+++ src/App.js
@@ -1,29 +1,42 @@
 import { useState,useRef } from "react";
 import TodoList from "./TodoList";
+import { v4 as uuid } from "uuid";
 
 function App() {
-  const [todos, setTodos] = useState([
-    { id: 1, name : "Todo1", completed: false}
-  ]);
+  const [todos, setTodos] = useState([]);
 
 const todoNameRef = useRef();
 
 const handleAddTodo = () => {
   //タスクを追加する
   const name = todoNameRef.current.value;
+  if(name === "") return;
   setTodos((prevTodos) => {
-    return [...prevTodos,{id: "1" ,name:name, completed: false}]
+    return [...prevTodos,{id: uuid(),name:name, completed: false}]
   });
   todoNameRef.current.value = null;
 };
 
+
+const toggleTodo = (id) => {
+  const newTodos = [...todos];
+  const todo = newTodos.find((todo) => todo.id === id);
+  todo.completed =!todo.completed;
+  setTodos(newTodos);
+};
+
+const handleClear = () => {
+  const newTodos = todos.filter((todo) => !todo.completed);
+  setTodos(newTodos);
+};
+
   return (
     <>
-    <TodoList todos={todos} />
+    <TodoList todos={todos} toggleTodo={toggleTodo}/>
     <input type="text" ref={todoNameRef}/>
     <button onClick={handleAddTodo}>タスクを追加</button> 
-    <button>完了したタスクの削除</button>
-    <div>残りのタスク:0</div>
+    <button onClick={handleClear}>完了したタスクの削除</button>
+    <div>残りのタスク:{todos.filter((todo) => !todo.completed).length}</div>
     </>
   );  
 }
